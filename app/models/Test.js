03e8fbd0fa03c68b6bd55e662a273e2934f0dbe9
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-02-18 22:01:40 +0630

    remove models

--- app/models/Test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class Test extends Parse.Object {
-  constructor() {
-    super('Test');
-  }
-}
-
-module.exports = Test;
--- cloud/main.js
+++ cloud/main.js
@@ -2,6 +2,5 @@
 
 require('../database/schemas/schemas');
 require('../database/triggers/triggers');
-require('../app/models/models');
 require('./functions/functions');
 require('./jobs/jobs');

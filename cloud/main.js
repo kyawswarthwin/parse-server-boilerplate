e771e478979c4945b023ef63b7793afaafc203de
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-02-20 21:28:41 +0630

    bugfix

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('../database/schemas/schemas');
-require('../database/triggers/triggers');
 require('../database/seeds/seeds');
 require('./functions/functions');
 require('./jobs/jobs');
+require('./triggers/triggers');

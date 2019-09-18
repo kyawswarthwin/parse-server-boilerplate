2df4d141663e82b7a89dd0ed5f92eb8a4519af9a
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2019-09-19 03:52:42 +0630

    remove db

--- cloud/main.js
+++ cloud/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-require('../database/schemas/schemas');
-require('../database/seeds/seeds');
 require('./functions/functions');
 require('./jobs/jobs');
 require('./triggers/triggers');
--- cloud/triggers/Test.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-  className: 'Test',
-  requiredKeys: ['name']
-};

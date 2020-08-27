f8283d63557317fa0d85cb376fafb9f0e8c4a359
kyawswarthwin <myanmarunicorn@gmail.com>
2020-08-27 06:48:28 +0630

    relation

--- src/cloud/triggers/Business.ts
+++ src/cloud/triggers/Business.ts
@@ -1,7 +1,10 @@
 import { BeforeSaveTrigger, requireLogin } from './triggers';
 
-export class Test implements BeforeSaveTrigger {
+export class Business implements BeforeSaveTrigger {
   beforeSave(req: Parse.Cloud.BeforeSaveRequest): void {
     requireLogin(req);
+
+    const relation = req.object.relation('users');
+    relation.add(req.user);
   }
 }

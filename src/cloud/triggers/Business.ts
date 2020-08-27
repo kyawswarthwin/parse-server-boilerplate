6e34efd7fc56b5916362b94368da0d15b7cb61b4
kyawswarthwin <myanmarunicorn@gmail.com>
2020-08-27 08:13:38 +0630

    fix

--- src/cloud/triggers/Business.ts
+++ src/cloud/triggers/Business.ts
@@ -4,37 +4,37 @@ export class Business implements BeforeSaveTrigger, AfterSaveTrigger {
   beforeSave(req: Parse.Cloud.BeforeSaveRequest): void {
     requireLogin(req);
 
-    const business = req.object;
-    if (business.existed()) {
+    const { user, object } = req;
+    if (object.existed()) {
       return;
     }
 
-    const relation = business.relation('users');
-    relation.add(req.user);
+    const relation = object.relation('users');
+    relation.add(user);
   }
 
   async afterSave(req: Parse.Cloud.BeforeSaveRequest): Promise<void> {
-    const business = req.object;
-    if (business.existed()) {
+    const { user, object } = req;
+    if (object.existed()) {
       return;
     }
 
     const roleACL = new Parse.ACL();
     roleACL.setPublicReadAccess(true);
 
-    const adminRole = new Parse.Role(`${business.id}_admin`, roleACL);
-    adminRole.getUsers().add(req.user);
+    const adminRole = new Parse.Role(`${object.id}_admin`, roleACL);
+    adminRole.getUsers().add(user);
     await adminRole.save();
 
-    const operatorRole = new Parse.Role(`${business.id}_operator`, roleACL);
+    const operatorRole = new Parse.Role(`${object.id}_operator`, roleACL);
     operatorRole.getRoles().add(adminRole);
     await operatorRole.save();
 
     const businessACL = new Parse.ACL();
     businessACL.setPublicReadAccess(true);
     businessACL.setRoleWriteAccess(adminRole, true);
-    business.setACL(businessACL);
-    await business.save(
+    object.setACL(businessACL);
+    await object.save(
       {},
       {
         useMasterKey: true,

ee6b04b423c914c4186043fccb5e849bf6d4c93c
kyawswarthwin <myanmarunicorn@gmail.com>
2020-08-27 07:36:37 +0630

    z

--- src/cloud/triggers/Business.ts
+++ src/cloud/triggers/Business.ts
@@ -4,23 +4,27 @@ export class Business implements BeforeSaveTrigger, AfterSaveTrigger {
   beforeSave(req: Parse.Cloud.BeforeSaveRequest): void {
     requireLogin(req);
 
+    const business = req.object;
+
     const acl = new Parse.ACL();
     acl.setPublicReadAccess(true);
-    req.object.setACL(acl);
+    business.setACL(acl);
 
-    const relation = req.object.relation('users');
+    const relation = business.relation('users');
     relation.add(req.user);
   }
 
   async afterSave(req: Parse.Cloud.BeforeSaveRequest): Promise<void> {
+    const business = req.object;
+
     const acl = new Parse.ACL();
     acl.setPublicReadAccess(true);
 
-    const adminRole = new Parse.Role(`${req.object.id}_admin`, acl);
+    const adminRole = new Parse.Role(`${business.id}_admin`, acl);
     adminRole.getUsers().add(req.user);
     await adminRole.save();
 
-    const operatorRole = new Parse.Role(`${req.object.id}_operator`, acl);
+    const operatorRole = new Parse.Role(`${business.id}_operator`, acl);
     operatorRole.getRoles().add(adminRole);
     await operatorRole.save();
   }

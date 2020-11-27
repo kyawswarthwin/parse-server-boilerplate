c36cc68a09ba8b66d3e82c4cf1970e9f3c14557c
Kyaw Swar Thwin <myanmarunicorn@gmail.com>
2020-11-27 21:19:01 +0630

    x

--- src/cloud/triggers/Category.ts
+++ src/cloud/triggers/Category.ts
@@ -4,10 +4,10 @@ import { uniqueKeys } from '../utils/common';
 export class Category implements BeforeSaveTrigger {
   async beforeSave(req: Parse.Cloud.BeforeSaveRequest): Promise<void> {
     const { object } = req;
+
     if (object.existed()) {
       return;
     }
-
     await uniqueKeys(req, ['name']);
 
     const acl = new Parse.ACL();
